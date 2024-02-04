9aed643395ae294f64175344fa5943e55f52a156
unknown <1224428720@qq.com>
2024-02-04 22:09:09 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,20 @@
   <div id="app">
     <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-shipin"></i>
+    </div>
+    <van-button>默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <van-cell-group>
+      <van-cell title="标题" value="内容" />
+      <van-cell title="标题" value="内容" label="详细信息" />
+      <van-cell title="标题" label="详细信息" is-link center />
+    </van-cell-group>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 // 加载全局样式
 import './styles/index.less'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(Vant)
+  .mount('#app')
